3237396915323ec5023d8b4e3a7f5ae8f22ac1d0
Andrew Thyng <andrew@thyng.dev>
2020-10-01 09:49:52 -0500

    Added fastify dependency

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('nothing here  yet')
+import fastify from 'fastify'
+
+const app = fastify({ logger: true })
+
+app.get('/', async (request, reply) => {
+  return reply.code(200).send()
+})
+
+const bootstrap = async () => {
+  try {
+    await app.listen(3000, '0.0.0.0')
+  } catch (err) {
+    app.log.error(err)
+  }
+}
+
+bootstrap()
